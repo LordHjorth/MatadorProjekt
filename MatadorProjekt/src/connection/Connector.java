533373c83c706f2978250cd3c3fc886e47a8c4a8
nicolasvahman <nicolasvahman@nicolass-mbp.eduroam.wireless.dtu.dk>
2018-04-10 10:44:04 +0200

    Connector klasse

--- MatadorProjekt/src/connection/Connector.java
+++ MatadorProjekt/src/connection/Connector.java
@@ -2,26 +2,60 @@ package connection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Connector {
 	
-	private static String dbUrl = "jdbc:mysql://localhost/PiSU";
-	private static String dbUsername = "gruppe25";
-	private static String dbPassword = "25";
+public static void main(String[] args) throws Exception {
+	
+		createTable();
+		
+	}
 
-	public Connection dbConnect() {
-		Connection connection = null;
+	public static Connection getConnection() throws Exception {
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
-			return connection;
-		} catch (SQLException e) {
-			System.err.print(e.getMessage() + " Noob - ingen sql bandit at hente her!");
+			String driver = "com.mysql.jdbc.Driver";
+			String url = "jdbc:mysql://10.16.175.65:3306/" + "matadorspil" + "?useSSL=false";
+			
+			String username = "gruppe25";
+			String password = "25";
+			
+			Class.forName(driver);
+			
+			
+			Connection conn = DriverManager.getConnection(url, username, password);
+			
+			System.out.println("Succesfuld tilslutning");
+			return conn;
 		} catch (Exception e) {
-			System.err.print(e.getMessage() + " Noob - nu er du bare en meget grim og dum abe :-)");
+			System.out.println(e);
 		}
+
+		System.out.println("Tilslutningen er fejlet");
 		return null;
+	}
+
+	public static void createTable() throws Exception {
+
+		try {
+//			getConnection();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		try {
+			Connection con = getConnection();
+			PreparedStatement create = con.prepareStatement(
+					"Create Table IF NOT EXISTS Testtabel (Landenavn varchar(255), Forkortelse varchar(255), Kontinent Varchar(255))");
+			create.executeUpdate();
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			System.out.println("Tabel lavet");
+		}
+
 	}
 
 }
