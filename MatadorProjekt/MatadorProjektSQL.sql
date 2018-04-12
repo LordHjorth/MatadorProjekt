435ad987405217b59d0e3f944fba85d6e01c3124
LordHjorth <hjorthen96@hotmail.com>
2018-04-12 10:27:39 +0200

    DB dump

---
+++ MatadorProjekt/MatadorProjektSQL.sql
@@ -0,0 +1,229 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+--
+-- Host: localhost    Database: MatadorProjekt
+-- ------------------------------------------------------
+-- Server version	5.6.39
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Car`
+--
+
+DROP TABLE IF EXISTS `Car`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Car` (
+  `color` varchar(10) CHARACTER SET utf8 NOT NULL,
+  `position` int(11) DEFAULT NULL,
+  PRIMARY KEY (`color`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Car`
+--
+
+LOCK TABLES `Car` WRITE;
+/*!40000 ALTER TABLE `Car` DISABLE KEYS */;
+INSERT INTO `Car` VALUES ('Black',0),('Blue',0),('Green',0),('Pink',0),('Red',0),('Yellow',0);
+/*!40000 ALTER TABLE `Car` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Game`
+--
+
+DROP TABLE IF EXISTS `Game`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Game` (
+  `id` int(11) NOT NULL,
+  `time_start` datetime DEFAULT NULL,
+  `time_end` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Game`
+--
+
+LOCK TABLES `Game` WRITE;
+/*!40000 ALTER TABLE `Game` DISABLE KEYS */;
+INSERT INTO `Game` VALUES (1,'2018-04-12 09:56:34',NULL);
+/*!40000 ALTER TABLE `Game` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Player`
+--
+
+DROP TABLE IF EXISTS `Player`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Player` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) CHARACTER SET utf8 DEFAULT NULL,
+  `carColor` varchar(10) CHARACTER SET utf8 DEFAULT NULL,
+  `inPrison` bit(1) DEFAULT NULL,
+  `balance` int(11) DEFAULT NULL,
+  `havePardon` bit(1) DEFAULT NULL,
+  `gameID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `Player_Info_CarColor_FK` (`carColor`),
+  KEY `game_GameID_FK` (`gameID`),
+  CONSTRAINT `Player_Info_CarColor_FK` FOREIGN KEY (`carColor`) REFERENCES `Car` (`color`),
+  CONSTRAINT `game_GameID_FK` FOREIGN KEY (`gameID`) REFERENCES `Game` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Player`
+--
+
+LOCK TABLES `Player` WRITE;
+/*!40000 ALTER TABLE `Player` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Player` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Property`
+--
+
+DROP TABLE IF EXISTS `Property`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Property` (
+  `id` int(11) NOT NULL,
+  `owned` bit(1) NOT NULL,
+  `ownedBy` int(11) DEFAULT NULL,
+  `houses` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `Property_PlayerID_FK` (`ownedBy`),
+  CONSTRAINT `Property_PlayerID_FK` FOREIGN KEY (`ownedBy`) REFERENCES `Player` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Property`
+--
+
+LOCK TABLES `Property` WRITE;
+/*!40000 ALTER TABLE `Property` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Property` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'MatadorProjekt'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `CreatePlayerID` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `CreatePlayerID`(IN id int, IN name nvarchar(30), IN color nvarchar(10), IN prison bit, IN balance int, IN pardon bit, IN gameID int)
+BEGIN
+
+INSERT INTO Player VALUES(id, name, color, prison, balance, pardon, gameID);
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `hej` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `hej`()
+BEGIN
+
+insert into Game Values(2, NOW(), null);
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `newGame` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `newGame`()
+BEGIN
+create table Game(id int primary key, time_start datetime, time_end datetime);
+
+create table Car(color nvarchar(10) primary key, position int);
+
+create table Player(id int primary key, name nvarchar (30), carColor nvarchar(10), inPrison bit, balance int, havePardon bit,  gameID int, constraint Player_Info_CarColor_FK foreign key (carColor) references Car(color), constraint game_GameID_FK foreign key (GameID) references Game(id));
+
+create table Property(id int primary key, owned bit not null, ownedBy int, houses int, constraint Property_PlayerID_FK foreign key (ownedBy) references Player(id));
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `UpdatePlayer` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `UpdatePlayer`(IN name nvarchar(30), IN prison bit, IN balance int, IN pardon bit, IN gameID int)
+BEGIN
+
+UPDATE Player set Player.inPrison = prison, Player.balance = balance, Player.havePardon = pardon where Player.name = name;
+
+SELECT gameID, name, Car.position, balance, inPrison, havePardon, color FROM Player INNER JOIN Car on Player.carColor = Car.color;
+
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-04-12 10:17:56
